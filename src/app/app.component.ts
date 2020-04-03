59063468973785920ce75b8599b2fc6e7ec92b5f
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2020-04-03 16:48:15 -0500

    Inclusion Estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { tweet } from './model/tweet.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./tweetStyles.css']
 })
 
 export class AppComponent {
